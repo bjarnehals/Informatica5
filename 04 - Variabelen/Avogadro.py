affdf495cf0f9e2a889cdb088309620be3435060
bjarnehals <42997593+bjarnehals@users.noreply.github.com>
2018-09-19 18:38:48 +0200

    wet van Coulomb

---
+++ 04 - Variabelen/Avogadro.py
@@ -0,0 +1,22 @@
+#deeltjes vragen
+nX = float(input('geef het aantal deeltjes zwavel: '))
+
+#gegevens
+MX = 32.06
+Na = 6.020 * (10 ** 23)
+
+#formules
+mX = MX * nX
+NX = nX * Na
+
+#uitvoer
+print(mX)
+print(NX)
+
+
+
+
+
+
+
+
