5517f5db24d14f9415bf12e0e36467e677e23e81
bjarnehals <42997593+bjarnehals@users.noreply.github.com>
2018-09-19 19:59:20 +0200

    wisselgeld 2

--- 04 - Variabelen/wisselgeld.py
+++ 04 - Variabelen/wisselgeld.py
@@ -18,3 +18,11 @@ n = l % 1
 muntstukken = a + c + e + g + i + k + m
 
 print(str(C) + ' cent kan je omwisselen in ' + str(muntstukken) + ' muntstukken')
+print(str(a) + ' muntstukken van 1 euro')
+print(str(c) + ' muntstukken van 50 cent')
+print(str(e) + ' muntstukken van 20 cent')
+print(str(g) + ' muntstukken van 10 cent')
+print(str(i) + ' muntstukken van 5 cent')
+print(str(k) + ' muntstukken van 2 cent')
+print(str(m) + ' muntstukken van 1 cent')
+print((str(float(C * (10 ** (-2))))) + ' euro in totaal')
