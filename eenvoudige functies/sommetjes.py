3dbf979111b7e9af0584d9cabd0abacd52b35ca9
bjarnehals <42997593+bjarnehals@users.noreply.github.com>
2018-10-03 10:37:41 +0200

    stelling van pythagoras

--- eenvoudige functies/sommetjes.py
+++ eenvoudige functies/sommetjes.py
@@ -1,14 +1,10 @@
 a = int(input('geef a: '))
 b = int(input('geef b: '))
 
-som1 = a + b
-som2 = (a * 10) + (b * 10)
-som3 = (a * 100) + (b * 100)
-som4 = (a * 1000) + (b * 1000)
-som5 = (a * 10000) + (b * 10000)
+c = a + b
 
-print('{:>6d}'.format(a) + ' + ' + '{:<6d}'.format(b) + ' = ' + '{:<6d}'.format(som1))
-print('{:>6d}'.format(a * 10) + ' + ' + '{:<6d}'.format(b * 10) + ' = ' + '{:<6d}'.format(som2))
-print('{:>6d}'.format(a * 100) + ' + ' + '{:<6d}'.format(b * 100) + ' = ' + '{:<6d}'.format(som3))
-print('{:>6d}'.format(a * 1000) + ' + ' + '{:<6d}'.format(b * 1000) + ' = ' + '{:<6d}'.format(som4))
-print('{:>6d}'.format(a * 10000) + ' + ' + '{:<6d}'.format(b * 10000) + ' = ' + '{:<6d}'.format(som5))
+print('{:>6d} + {:<6d} = {:<d}'.format(a,b,c))
+print('{:>6d} + {:<6d} = {:<d}'.format(a * 10,b * 10,c * 10))
+print('{:>6d} + {:<6d} = {:<d}'.format(a * 100,b * 100,c * 100))
+print('{:>6d} + {:<6d} = {:<d}'.format(a * 1000,b * 1000,c * 1000))
+print('{:>6d} + {:<6d} = {:<d}'.format(a * 10000,b * 10000,c * 10000))
