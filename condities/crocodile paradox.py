66824ade29bf46cdee9c2b2df434b02932f506c9
bjarnehals <42997593+bjarnehals@users.noreply.github.com>
2018-10-03 11:39:35 +0200

    stelling van pythagoras

---
+++ condities/crocodile paradox.py
@@ -0,0 +1,10 @@
+naam = input('Wie heeft stalen zenuwen?: ')
+
+if naam == 'vader':
+    boodschap = 'Krokodil geeft kind terug'
+elif naam == 'krokodil':
+    boodschap = 'Krokodil eet kind op'
+else:
+    boodschap = 'Moe van het denken'
+
+print(boodschap)
