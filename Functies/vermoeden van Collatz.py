dfca9d7c2b17b485b713fd8409e9dde0418fa21b
bjarnehals <42997593+bjarnehals@users.noreply.github.com>
2019-01-14 17:31:47 +0100

    vermoeden van collatz

--- Functies/vermoeden van Collatz.py
+++ Functies/vermoeden van Collatz.py
@@ -1,12 +1,24 @@
-def volgend_getal_collatz(getal):
+def volgend_collatz_getal(getal):
 
     if getal % 2 == 0:
         getal /= 2
     else:
         getal = (getal * 3) + 1
-    return getal
+    return int(getal)
 
-print(volgend_getal_collatz(27))
+def collatz(getal):
+
+    cyclelengte = 0
+
+    while getal >= 1:
+        cyclelengte += 1
+        if getal != 1:
+            getal = volgend_collatz_getal(getal)
+        else:
+            return int(cyclelengte)
+
+
+
 
 
 
